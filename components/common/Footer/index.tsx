493d07801a782734d63b7fa927c1bad4393062b5
davien21 <chidiebereekennia@gmail.com>
2024-01-21 14:52:15 +0100

    --fix: deployment

--- components/common/Footer/index.tsx
+++ components/common/Footer/index.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 
 import styles from "./footer.module.scss";
-import { Button } from "components";
-import {
-  InstagramIcon,
-  LogoIcon,
-  SpotifyIcon,
-  TiktokIcon,
-  YoutubeIcon,
-} from "assets/images";
 
 const currentYear = new Date().getFullYear();
 
 function Footer() {
   return (
-    <footer className={`${styles["container"]} bg-bl ack-2 text- white text-center`}>
-      Footer
+    <footer
+      className={`${styles["container"]} bg-bl ack-2 text- white text-center`}
+    >
+      Capter AI &copy; {currentYear}
     </footer>
   );
 }
